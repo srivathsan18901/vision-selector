bca8859303ff8dec6020c8e64a38887749cc904f
Srivathsan Ganesan <srivathsan18901@gmail.com>
2026-02-07 14:30:52 +0530

    new UI changes

--- vision-frontend/src/pages/home.tsx
+++ vision-frontend/src/pages/home.tsx
@@ -1,30 +1,70 @@
-import React from 'react'
-import InputForm from '../components/inputForm/InputForm'
-import Recommendations from '../components/recommendations/recommendations'
-import Results from '../components/result/result';
-import { useState } from 'react';
+import React, { useRef, useState } from "react";
+import InputForm from "../components/inputForm/InputForm";
+import Recommendations from "../components/recommendations/recommendations";
+import Results from "../components/result/result";
+import HeroVid from "../assets/Main_video/Industrial_Vision_System_Video_Generation.mp4"
 
-
-function Home() {  
+function Home() {
   const [result, setResult] = useState<any>(null);
   const [input, setInput] = useState<any>(null);
+  const [showForm, setShowForm] = useState(false);
+
+  const formRef = useRef<HTMLDivElement>(null);
+
+  const handleGetStarted = () => {
+    setShowForm(true);
+
+    // wait for form to render, then scroll
+    setTimeout(() => {
+      formRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  };
 
   return (
     <div className="container">
-    <div className="header">
-      <h1>Vision System Selector</h1>
-      <p className="subtitle">
-        Intelligent camera & lens selection for machine vision systems
-      </p>
-    </div>
+      {/* HERO SECTION */}
+      <div className="hero">
+        <div className="hero-left">
+          <span className="badge-pill">🚀 Smart Vision selection Tool</span>
 
-      <InputForm onCalculate={(data, form) => {
-        setResult(data);
-        setInput(form);
-      }} />
+          <h1>BGR Neo <br/> Vision System Selector</h1>
 
+          <p className="subtitle">
+            Intelligent camera & lens selection for machine vision systems
+          </p>
 
-      {result && (
+          <button className="cta-btn" onClick={handleGetStarted}>
+            Get Started
+          </button>
+        </div>
+
+        <div className="hero-right">
+          <video
+            className="hero-video"
+            src={HeroVid}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        </div>
+
+      </div>
+
+      {/* INPUT FORM (VISIBLE ONLY AFTER CLICK) */}
+      {showForm && (
+        <div ref={formRef}>
+          <InputForm
+            onCalculate={(data: any, form: any) => {
+              setResult(data);
+              setInput(form);
+            }}
+          />
+        </div>
+      )}
+
+      {/* RESULTS */}
+      {showForm && result && (
         <>
           <Results data={result.calculation} input={input} />
           <Recommendations data={result.recommendations} />
@@ -34,4 +74,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home;
