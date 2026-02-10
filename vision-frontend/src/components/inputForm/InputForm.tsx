15b1b7acef14126bc918028b7c8cd2e10eebd681
Srivathsan Ganesan <srivathsan18901@gmail.com>
2026-02-10 12:17:29 +0530

    UI Changes

--- vision-frontend/src/components/inputForm/InputForm.tsx
+++ vision-frontend/src/components/inputForm/InputForm.tsx
@@ -3,7 +3,6 @@ import "./InputForm.css";
 import type { InputFormProps, VisionInput } from "./InputForm.types";
 import { toast } from "react-toastify";
 
-
 const InputForm = ({ onCalculate }: InputFormProps) => {
   const [form, setForm] = useState<VisionInput>({
     componentWidth: 0,
@@ -21,79 +20,83 @@ const InputForm = ({ onCalculate }: InputFormProps) => {
     });
   };
 
-const handleSubmit = async () => {
-  if (form.componentWidth <= 0 || form.componentHeight <= 0 || form.workingDistance <= 0 || form.accuracy <= 0) {
-    toast.error("Please fill in all fields with valid values.");
-    return;
-  }
-
-  try {
-    const response = await fetch(
-      "http://localhost:5000/api/vision/calculate",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      }
-    );
-
-    if (response.ok) {
-     toast.success("Vision parameters calculated successfully");
-    }
-
-    if (!response.ok) {
-      toast.error("Backend service unavailable");
+  const handleSubmit = async () => {
+    if (
+      form.componentWidth <= 0 ||
+      form.componentHeight <= 0 ||
+      form.workingDistance <= 0 ||
+      form.accuracy <= 0
+    ) {
+      toast.error("Please fill in all fields with valid values.");
       return;
     }
 
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/vision/calculate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        },
+      );
 
-    // Send backend result to Home.tsx
-    onCalculate(data, form);
+      if (response.ok) {
+        toast.success("Vision parameters calculated successfully");
+      }
 
-  } catch (error) {
-    toast.error("Failed to calculate vision parameters");
-    return
-  }
-};
+      if (!response.ok) {
+        toast.error("Backend service unavailable");
+        return;
+      }
+
+      const data = await response.json();
+
+      // Send backend result to Home.tsx
+      onCalculate(data, form);
+    } catch (error) {
+      toast.error("Failed to calculate vision parameters");
+      return;
+    }
+  };
 
   return (
-  <div className="section">
-    <h2>Input Parameters</h2>
+    <div className="section">
+      <h2>Input Parameters</h2>
 
-    <div className="form-grid">
-      <div className="form-group">
-        <label>Component Width (mm)</label>
-        <input type="number" name="componentWidth" onChange={handleChange} />
+      <div className="form-grid">
+        <div className="form-group">
+          <label>Component Width (mm)</label>
+          <input type="number" name="componentWidth" onChange={handleChange} />
+        </div>
+
+        <div className="form-group">
+          <label>Component Height (mm)</label>
+          <input type="number" name="componentHeight" onChange={handleChange} />
+        </div>
+
+        <div className="form-group">
+          <label>Working Distance (mm)</label>
+          <input type="number" name="workingDistance" onChange={handleChange} />
+        </div>
+
+        <div className="form-group">
+          <label>Required Accuracy (µm)</label>
+          <input type="number" name="accuracy" onChange={handleChange} />
+        </div>
+
+        <div className="toggle">
+          <input type="checkbox" name="color" onChange={handleChange} />
+          <span>Color Camera</span>
+        </div>
       </div>
 
-      <div className="form-group">
-        <label>Component Height (mm)</label>
-        <input type="number" name="componentHeight" onChange={handleChange} />
-      </div>
-
-      <div className="form-group">
-        <label>Working Distance (mm)</label>
-        <input type="number" name="workingDistance" onChange={handleChange} />
-      </div>
-
-      <div className="form-group">
-        <label>Required Accuracy (µm)</label>
-        <input type="number" name="accuracy" onChange={handleChange} />
+      <div className="form-actions">
+        <button onClick={handleSubmit}>Calculate</button>
       </div>
     </div>
-
-    <div className="form-actions">
-      <div className="toggle">
-        <input type="checkbox" name="color" onChange={handleChange} />
-        <span>Color Camera</span>
-      </div>
-
-      <button onClick={handleSubmit}>Calculate</button>
-    </div>
-  </div>
   );
 };
 
